14d94ffc36b65e095989a3c0959f10ec011bb8e7
tomislavantebrncic <tomislav.brncic@demandforce.net>
2025-01-10 13:49:18 +0100

    Working

--- Workflow/Client/Program.cs
+++ Workflow/Client/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Another test for workflow.");
+Console.WriteLine("Press any key to exit...");
